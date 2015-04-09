9110b1eb85c8f104b7aa39ba83f80678fdd4ef8e
Titome <github@timotheegirard.com>
2015-04-09 23:35:42 +0200

    fixit not good name of variable for admin configuration

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_tuto',
+        'NAME': 'djangotuto18',
         'USER': 'dev',
         'PASSWORD': 'dev',
     }
